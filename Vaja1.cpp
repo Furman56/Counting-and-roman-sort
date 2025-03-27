2bf213843a0d3c36de483925f9c2b01554890113
Furman56 <furman.urban@gmail.com>
2025-03-27 20:32:37 +0100

    Added initial implementation of Counting Sort

--- Vaja1.cpp
+++ Vaja1.cpp
@@ -26,6 +26,35 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 		output << polje[i] << ' ';
 }
 
+void CountingSort(vector<int>& A) {
+	if (A.empty()) return;
+
+	int max = *max_element(A.begin(), A.end());
+	int min = *min_element(A.begin(), A.end());
+	int range = max - min + 1;
+
+	vector<int> C(range, 0);
+
+	for (int num : A) {
+		C[num]++;
+	}
+
+	for (int i = 1; i < C.size(); i++) {
+		C[i] += C[i - 1];
+	}
+
+	vector<int> B(A.size());
+
+	for (int i = A.size() - 1; i >= 0; i--) {
+		B[C[A[i]] - 1] = A[i];
+		C[A[i]]--;
+	}
+
+	Izpis_Stevil(&A[0], A.size());
+
+	cout << "Counting sort done." << endl;
+}
+
 int main(int argc, const char* argv[]) {
 	vector<int> A;
 
